29886d52ebabadb6e3b6baa2bd4cd2e512342b72
yeonoel <yeopevroguinoel@gmail.com>
2023-02-07 20:53:02 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,8 +21,13 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandle(401)
+def unauthorized(error) -> str:
+    """ unauthorized handle. """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
