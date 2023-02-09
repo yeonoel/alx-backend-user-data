db3a931a81bb8b13bd8a24a8990a79590e93e38e
yeonoel <yeopevroguinoel@gmail.com>
2023-02-09 22:20:10 +0000

    update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,7 +59,8 @@ def before_request():
 
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+		      '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
