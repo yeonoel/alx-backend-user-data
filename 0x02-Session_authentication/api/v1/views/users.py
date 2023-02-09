718c7551ae1bafe8da24cf3f09b3b078577edd05
yeonoel <yeopevroguinoel@gmail.com>
2023-02-09 22:24:45 +0000

    Update user

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -44,6 +44,12 @@ def delete_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id == 'me' and request.current_user is None:
+        abort(404)
+
+    if user_id == 'me' and request.current_user is not None:
+        return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
     if user is None:
         abort(404)
