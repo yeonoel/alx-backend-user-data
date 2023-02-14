b258aa11be6102e565e8f0c247807c4073539179
yeonoel <yeopevroguinoel@gmail.com>
2023-02-14 12:08:44 +0000

    Update

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,6 +27,13 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+
+    if user_id == 'me' and request.current_user is None:
+        abort(404)
+
+    if user_id == 'me' and request.current_user is not None:
+        return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
     if user is None:
         abort(404)
@@ -44,12 +51,6 @@ def delete_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    if user_id == 'me' and request.current_user is None:
-        abort(404)
-
-    if user_id == 'me' and request.current_user is not None:
-        return jsonify(request.current_user.to_json())
-
     user = User.get(user_id)
     if user is None:
         abort(404)
@@ -126,4 +127,3 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
-
