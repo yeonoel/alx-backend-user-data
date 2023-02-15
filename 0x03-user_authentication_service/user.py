3d499d624b3d3ae727ee6e3ad955623e4ad094fc
yeonoel <yeopevroguinoel@gmail.com>
2023-02-15 16:07:42 +0000

    Mdel user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,6 +2,8 @@
 """
 User Model
 """
+
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,8 +13,10 @@ Base = declarative_base()
 class User(Base):
     """ User Model """
     __tablename__ = 'users'
+   
+
     id = Column(Integer, primary_Key=True)
-    name = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String(225))
-    reset_token = Column(String(225))
+    name = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
