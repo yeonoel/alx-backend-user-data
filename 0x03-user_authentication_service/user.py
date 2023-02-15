e8695fce5bf45b036ff92d1ed21b8fa5044285be
yeonoel <yeopevroguinoel@gmail.com>
2023-02-15 16:17:10 +0000

    Mdel user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,10 +13,10 @@ Base = declarative_base()
 class User(Base):
     """ User Model """
     __tablename__ = 'users'
-   
 
     id = Column(Integer, primary_Key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
     reset_token = Column(String(250))
